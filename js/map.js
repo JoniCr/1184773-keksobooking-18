cd6c2f99c448a90aa7e7d5276f8254a556342697
JoniCR <business_evgeniy_04@mail.ru>
2019-10-12 18:49:00 +0300

    deleted map.js

--- js/map.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-  function inactiveMap() {
-    window.data.map.classList.add('map--faded');
-    window.data.adForm.classList.add('ad-form--disabled');
-  }
-  inactiveMap();
-})();
